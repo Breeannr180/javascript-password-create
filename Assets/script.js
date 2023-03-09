479dd02befdc0e0fb4f62685214c3ea274b97fb7
Breeannr180 <breeannr@hotmail.com>
2023-03-09 14:08:34 -0700

    changes

--- Assets/script.js
+++ Assets/script.js
@@ -40,4 +40,3 @@ generateBtn.addEventListener("click", writePassword);
 // WHEN the password is generated
 // THEN the password is either displayed in an alert or written to the page
 // ```
-// test test 
